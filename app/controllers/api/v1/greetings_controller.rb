dd286d7b2234e45853d41ebe35ea16f2ad39aa28
Kenny <kennysalazar3197@gmail.com>
2022-07-06 11:37:28 -0500

    Update CORS policy

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::GreetingsController < ApplicationController
   def index
     greeting = Greeting.pluck(:greeting).sample
 
+    headers['Access-Control-Allow-Origin'] = '*'
     render json: { message: greeting }
   end
 end
